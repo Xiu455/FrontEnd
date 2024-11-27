2abda0e90bd9171effd65c8d53f54de771a5a6f8
Xiu455 <abz0937649531@gmail.com>
2024-11-27 20:14:35 +0800

    update

--- src/1.瀑布流/1/WaterfallLayout.js
+++ src/1.瀑布流/1/WaterfallLayout.js
@@ -1,22 +1,26 @@
 export class WaterfallLayout{
+    #selector;
+    #mainElem;
+
     constructor(selector, args={
         columnWidth: 200,
         gap: 0
     }){
         const _ = args;
 
-        this.selector = selector;
-        this.mainElem = this.getMainElem();
+        this.#selector = selector;
+        this.#mainElem = this.#getMainElem();
 
-        this.monitor();
+        this.#monitor();
     }
 
-    monitor(){
-        window.addEventListener('resize', () => {
+    #monitor(){
+        window.addEventListener('resize', () => {   
+            
         });
     }
 
-    getMainElem(){
-        return document.querySelector(this.selector);
+    #getMainElem(){
+        return document.querySelector(this.#selector);
     }
 }
---
+++ src/1.瀑布流/1/main.js
@@ -0,0 +1,8 @@
+import { WaterfallLayout } from './WaterfallLayout.js';
+
+(() => {
+    const waterfall = new WaterfallLayout('#box1',{
+        columnWidth: 300,
+        gap: 10
+    });
+})();
