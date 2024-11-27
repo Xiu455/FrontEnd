9b84b448873cd9c22d36b51f2cc8c387f68cf5ac
Xiu455 <abz0937649531@gmail.com>
2024-11-27 20:26:45 +0800

    update

--- src/1.瀑布流/1/WaterfallLayout.js
+++ src/1.瀑布流/1/WaterfallLayout.js
@@ -1,26 +1,33 @@
 export class WaterfallLayout{
-    #selector;
-    #mainElem;
-
     constructor(selector, args={
         columnWidth: 200,
         gap: 0
     }){
-        const _ = args;
+        this.param = {
+            columnWidth: args.columnWidth,
+            gap: args.gap
+        }
+        this.selector = selector;
+        this.mainElem = this.getMainElem();
+        this.columnCount = this.getColumnCount();
 
-        this.#selector = selector;
-        this.#mainElem = this.#getMainElem();
-
-        this.#monitor();
+        this.monitor();
     }
 
-    #monitor(){
-        window.addEventListener('resize', () => {   
-            
+    monitor(){
+        window.addEventListener('resize', () => {
+            this.columnCount = this.getColumnCount();
+            console.log(this.columnCount);
         });
     }
 
-    #getMainElem(){
-        return document.querySelector(this.#selector);
+    getMainElem(){
+        return document.querySelector(this.selector);
+    }
+
+    getColumnCount(){
+        let width = this.mainElem.offsetWidth;
+        let columnCount = Math.floor(width / (this.param.columnWidth + this.param.gap));
+        return Math.floor(columnCount);
     }
 }
