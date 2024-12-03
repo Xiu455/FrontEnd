be9974203c0093b8d74978b02f2623f1306ced8d
Xiu455 <150805348+Xiu455@users.noreply.github.com>
2024-12-03 11:44:58 +0800

    update

--- src/1.瀑布流/1/WaterfallLayout.js
+++ src/1.瀑布流/1/WaterfallLayout.js
@@ -10,27 +10,37 @@ export class WaterfallLayout{
 
         // 初始化参数
         this.CSS_id = 'waterfall-layout-style-aB3dEf7G';
+        this.htmlContainer = [];
+
         this.selector = selector;                   // 主元素選擇器
         this.mainElem = this.getMainElem();         // 主元素
         this.columnCount = this.getColumnCount();   // 列数
-
-        console.log(this.columnCount);
+        this.columnIndex = 0;                       // 已布局列數
 
         this.init();
     }
 
+    /**
+     * 獲得主元素
+     * @returns {Element} 主元素
+     */
     getMainElem(){
         return document.querySelector(this.selector);
     }
 
+    /**
+     * 獲得列數
+     * @returns {number} 列數
+     */
     getColumnCount(){
         return Math.floor(this.mainElem.offsetWidth / (this.param.columnWidth + this.param.gap));
     }
 
     // 初始化
     init(){
-        this.waterfallCSS();
-        this.monitor();
+        this.waterfallCSS();    // 瀑布流CSS初始化
+        this.columnlayout();    // 瀑布流布局初始化
+        this.monitor();         // 監視器初始化
     }
 
     // 瀑布流布局CSS
@@ -51,14 +61,16 @@ export class WaterfallLayout{
             }
 
             ${this.selector} .itemColumn{
-                width: 100%;
-                min-width: ${this.param.columnWidth}px;
+                width: 100%; min-width: ${this.param.columnWidth}px;
+                height: max-content; min-height: 100px;
 
                 display: flex;
                 flex-direction: column;
                 justify-content: flex-start;
                 align-items: center;
                 flex-shrink: 1;
+
+                gap: ${this.param.gap}px;
             }
         `));
 
@@ -68,7 +80,43 @@ export class WaterfallLayout{
     // 監視器
     monitor(){
         window.addEventListener('resize', () => {
-            
+            this.columnCount = this.getColumnCount();
+            this.columnlayout();
         });
+    }
+
+    columnlayout(){
+        if(this.columnIndex === this.columnCount) return;
+
+        this.mainElem.innerHTML = '';
+
+        for(let i = 0; i < this.columnCount; i++){
+            let column = document.createElement('div');
+            column.classList.add('itemColumn');
+            this.mainElem.appendChild(column);
+        }
+
+        this.columnIndex = this.columnCount;
+    }
+
+    insertElem(html){
+        let columnElems = this.mainElem.querySelectorAll('& > .itemColumn');
+
+        let mainColumn = {
+            h: 0,
+            elem: null
+        };
+
+        columnElems.forEach((e) => {
+            if(mainColumn.elem === null){
+                mainColumn = {h: e.offsetHeight, elem: e}
+            }
+
+            if(e.offsetHeight < mainColumn.h){
+                mainColumn = {h: e.offsetHeight, elem: e}
+            }
+        });
+
+        mainColumn.elem.innerHTML += html;
     }
 }
--- src/1.瀑布流/1/main.js
+++ src/1.瀑布流/1/main.js
@@ -1,8 +1,33 @@
 import { WaterfallLayout } from './WaterfallLayout.js';
 
-(() => {
+(async () => {
     const waterfall = new WaterfallLayout('#box1',{
-        columnWidth: 200,
+        columnWidth: 300,
         gap: 10
     });
+
+
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    
+    const convertHTML = (data) => {
+        return /* html */`
+            <div class="data_box">
+                <img src=${data.img}>
+                <span>${data.text}<span>
+            </div>
+        `
+    }
+
+    let datas = [
+        {img: "https://picsum.photos/300/300?random=1", text: "1"},
+        {img: "https://picsum.photos/300/500?random=2", text: "2"},
+        {img: "https://picsum.photos/300/200?random=3", text: "3"},
+        {img: "https://picsum.photos/300/200?random=4", text: "4"},
+        {img: "https://picsum.photos/300/400?random=5", text: "5"},
+    ];
+
+    for(let data of datas){
+        waterfall.insertElem(convertHTML(data));
+        await delay(1000);
+    }
 })();
