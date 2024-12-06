d5dbb0d8cd3f24b09cb063c76805035dee73157a
Xiu455 <abz0937649531@gmail.com>
2024-12-06 19:47:38 +0800

    update

--- src/1.瀑布流/0/main.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    var elem = document.querySelector('.grid');
-    var msnry = new Masonry(elem, {
-        // options
-        itemSelector: '.grid-item',
-        gutter: 20,
-        percentPosition: true,
-    });
-})();
