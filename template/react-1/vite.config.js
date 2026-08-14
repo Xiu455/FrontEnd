d609f5e571cfd9ef3bc776b8999ab0426044776f
Xiu455 <150805348+Xiu455@users.noreply.github.com>
2026-08-14 16:49:03 +0800

    temp

--- template/react-1/vite.config.js
+++ template/react-1/vite.config.js
@@ -1,7 +1,27 @@
+// https://vite.dev/config/
+
+import { join } from 'path'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const ROOT = process.cwd();
+
+/** 
+ * @type {import('vite').UserConfig} 
+ */
+// eslint-disable-next-line no-unused-vars
+export default defineConfig(async ({ command, mode }) => {
+  return{
+    base: './',
+    plugins: [
+      react(),
+    ],
+    resolve: {
+      tsconfigPaths: true,
+    },
+    build: {
+      outDir: join(ROOT, 'dist')
+    },
+  }
+});
