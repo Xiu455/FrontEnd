8a5c1a896d8aa8a808ac6e73492d10b316af7f5b
Xiu455 <150805348+Xiu455@users.noreply.github.com>
2026-08-19 20:55:11 +0800

    up

--- viwe/react-src/src/App.jsx
+++ viwe/react-src/src/App.jsx
@@ -3,12 +3,50 @@ import { useQuery } from '@tanstack/react-query'
 
 import './App.scss'
 
-export default function App(){
-  
+const PAGES_ROOT = `https://xiu455.github.io/FrontEnd`;
 
-  return (<>
-    <div id="center">
-      AAA-123
+/**
+ * @param {string} name 
+ * @returns 
+ */
+const extractNumber = (name) => {
+  const match = name.match(/^(\d+)\./);
+  return match ? Number(match[1]) : Infinity;
+};
+
+const fetchData = async () => {
+  const res = await(await fetch(`https://api.github.com/repos/Xiu455/FrontEnd/contents/src`)).json();
+  const dirData = res.sort((a, b) => extractNumber(a.name) - extractNumber(b.name));
+
+  // console.log(dirData);
+
+  return dirData;
+}
+
+const Card = ({ data }) => {
+  // console.log(data);
+  const { name, path } = data;
+
+  return <a className='card' data-props-for="pointer-local" href={`${PAGES_ROOT}/${path}`} target="_blank">
+    <div className="bg" />
+    <div className='name'><span>{name}</span></div>
+  </a>
+}
+
+export default function App(){
+  const { data: datas, isLoading } = useQuery({
+    queryKey: ['data'],
+    queryFn: fetchData,
+  });
+
+  if(isLoading){ return <div>Loading...</div>}
+
+  return (<div id="center">
+    <div className="card-box">
+      {datas.map((data) => {
+        return(<Card key={data.name} data={data} />)
+      })}
     </div>
-  </>)
+
+  </div>)
 }
